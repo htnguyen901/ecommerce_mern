67fe4658778532c278e3b7148f368a301cee2761
Ha Nguyen <64000331+htnguyen901@users.noreply.github.com>
2021-05-07 13:39:13 +0700

    eighth commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
+const { send } = require('process');
 
 connectDB();
 
@@ -12,5 +14,17 @@ app.use(express.json());
 app.use('/api/products', productRoutes );
 
 const PORT = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req,res)=> {
+        res.sendFile(path.join(__dirname,'frontend','build','index.html'));
+    })
+} else {
+    app.get('/', (req,res) => {
+        res,send('API running');
+    });
+}
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
