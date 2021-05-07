9dc164484193faaed31e9674a8a99870b9ae3021
Ha Nguyen <64000331+htnguyen901@users.noreply.github.com>
2021-05-07 14:13:55 +0700

    13th commit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({path: '../dotenv'});
 const path = require('path');
 const express = require('express');
 const connectDB = require('./config/db');
