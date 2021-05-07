85f23d45ee781a19429fcbb17caa81524e8ba695
Ha Nguyen <64000331+htnguyen901@users.noreply.github.com>
2021-05-07 14:46:24 +0700

    16th commit

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,15 @@ app.use('/api/products', productRoutes );
 const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static('frontend/build'));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
     app.get('*', (req,res)=> {
-        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
+        res.sendFile(path.join(__dirname,'frontend','build','index.html'));
     })
+} else {
+    app.get('/', (req,res) => {
+        res.send('API running');
+    });
 }
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
