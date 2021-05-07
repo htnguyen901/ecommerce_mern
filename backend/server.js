809c680d6f934c86502a3086327ad0533746fc5b
Ha Nguyen <64000331+htnguyen901@users.noreply.github.com>
2021-05-07 14:20:36 +0700

    14th commit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config({path: '../dotenv'});
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const connectDB = require('./config/db');
@@ -15,10 +15,10 @@ app.use('/api/products', productRoutes );
 const PORT = process.env.PORT || 5000;
 
 if(process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.use(express.static('../frontend/build'));
 
     app.get('*', (req,res)=> {
-        res.sendFile(path.join(__dirname,'frontend','build','index.html'));
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'));
     })
 }
 
